ea8bb44d7bf9656571ab7d0215f6299934ba686d
PrimeLens <jhoedavis@gmail.com>
2016-12-19 15:17:09 -0500

    arrow functions

---
+++ arrowFunctions.js
@@ -0,0 +1,67 @@
+
+/***** Example 1 *****/
+
+	function calc(num){
+		return num+2;
+	}
+
+	var calc = function(num){
+		return num+2;
+	}
+
+// translates to
+	var calc = (num) => {return num+2;}
+// translates to
+	var calc = num => num+2;
+
+
+
+
+
+
+
+/***** Example 2 (similarly) *****/
+
+	function trxObj(data, num){
+		data.num = num;
+		return data;
+	}
+
+	var trxObj = function(data, num){
+		data.num = num;
+		return data;
+	}
+
+// translates to
+	var trxObj = (data, num) => {data.num = num; return data;}
+
+
+
+
+
+
+/***** Example 3  (var self = this) *****/
+var aaa = {
+	x : 10,
+	bbb : function(){
+		console.log('1: ',this.x); // prints 10
+		var calc = function(){
+			console.log('2: ', this.x); // prints undefined
+		}
+		calc();
+	}
+}
+aaa.bbb(); 
+
+// Translates to below, note the second console log this.x will print 10
+var aaa = {
+	x : 10,
+	bbb : function(){
+		console.log('1: ',this.x); // prints 10
+		var calc = () => {
+			console.log('2: ', this.x); // prints 10
+		}
+		calc();
+	}
+}
+
